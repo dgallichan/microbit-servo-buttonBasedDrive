2e53fac87cb8627934511327290655cbdf2abaf6
Daniel Gallichan <dgallichan@yahoo.com>
2023-03-02 12:19:16 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+function updateServo () {
+    servoValue = Math.constrain(servoValue, 0, 180)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, servoValue)
+}
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    servoValue = 90
+})
+input.onButtonPressed(Button.A, function () {
+    servoValue += -5
+})
+input.onButtonPressed(Button.B, function () {
+    servoValue += 5
+})
+let servoValue = 0
+servoValue = 90
+basic.showIcon(IconNames.Scissors)
+basic.forever(function () {
+    led.plotBarGraph(
+    servoValue,
+    180
+    )
+})
