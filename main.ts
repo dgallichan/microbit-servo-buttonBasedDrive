a24900da240c89837f98a7b94f9adb5cac5c3428
Daniel Gallichan <dgallichan@yahoo.com>
2023-03-06 15:56:07 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,40 @@
+buttonClicks.onButtonSingleClicked(buttonClicks.AorB.B, function () {
+    servoValue += 5
+    updateServo()
+})
 function updateServo () {
-    servoValue = Math.constrain(servoValue, 0, 180)
-    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, servoValue)
+	
 }
+buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.A, function () {
+    servoValue = 5
+    updateServo()
+})
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     servoValue = 90
     updateServo()
 })
-input.onButtonPressed(Button.A, function () {
+buttonClicks.onButtonHeld(buttonClicks.AorB.B, function () {
+    servoValue += 15
+    updateServo()
+})
+buttonClicks.onButtonSingleClicked(buttonClicks.AorB.A, function () {
     servoValue += -5
     updateServo()
 })
-input.onButtonPressed(Button.B, function () {
-    servoValue += 5
+buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.B, function () {
+    servoValue = 180
+    updateServo()
+})
+buttonClicks.onButtonHeld(buttonClicks.AorB.A, function () {
+    servoValue += -15
     updateServo()
 })
 let servoValue = 0
 servoValue = 90
 basic.showIcon(IconNames.Scissors)
 basic.forever(function () {
-    led.plotBarGraph(
-    servoValue,
-    180
-    )
+    servoValue = Math.constrain(servoValue, 0, 180)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, servoValue)
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "Servo: " + convertToText(servoValue))
+    basic.pause(200)
 })
